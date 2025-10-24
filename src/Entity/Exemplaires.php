30826a3fe94d4f0048024a4f9765a7a182cfaae1
huguitow <hdesj84@gmail.com>
2025-10-24 12:29:49 +0200

    Ajout de la classe Exemplaires et migration

---
+++ src/Entity/Exemplaires.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ExemplairesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ExemplairesRepository::class)]
+class Exemplaires
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 4)]
+    private ?string $cote = null;
+
+    #[ORM\Column(length: 40)]
+    private ?string $etat = null;
+
+    #[ORM\Column]
+    private ?bool $disponible = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCote(): ?string
+    {
+        return $this->cote;
+    }
+
+    public function setCote(string $cote): static
+    {
+        $this->cote = $cote;
+
+        return $this;
+    }
+
+    public function getEtat(): ?string
+    {
+        return $this->etat;
+    }
+
+    public function setEtat(string $etat): static
+    {
+        $this->etat = $etat;
+
+        return $this;
+    }
+
+    public function isDisponible(): ?bool
+    {
+        return $this->disponible;
+    }
+
+    public function setDisponible(bool $disponible): static
+    {
+        $this->disponible = $disponible;
+
+        return $this;
+    }
+}
