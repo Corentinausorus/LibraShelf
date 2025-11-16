f5d968af7c61e43eb301966fc51fb02bc11d7167
Corentin <coco.meersseman@gmail.com>
2025-11-16 14:25:10 +0100

    Creation de la premiere page affichant les ouvrages et leur caractéristiques

--- src/Controller/OuvrageController.php
+++ src/Controller/OuvrageController.php
@@ -5,14 +5,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\OuvrageRepository;
 
 final class OuvrageController extends AbstractController
 {
+    /*
     #[Route('/ouvrage', name: 'app_ouvrage')]
     public function index(): Response
     {
         return $this->render('ouvrage/index.html.twig', [
             'controller_name' => 'OuvrageController',
+        ]);
+    }
+        */
+
+    #[Route('/ouvrage', name: 'app_ouvrage_new')]
+    public function index(OuvrageRepository $ouvrageRepository): Response
+    {
+        return $this->render('ouvrage/index.html.twig', [
+            'ouvrages' => $ouvrageRepository->findAll(),
         ]);
     }
 }
