07a60ac3b7aa98eb9b6202941f1321d050290af7
Hugo DESJARDINS <hdesj84@gmail.com>
2025-11-25 01:22:55 +0100

    Rajout de l'obligation d'être admin pour UserRoleController

--- src/Controller/UserRoleController.php
+++ src/Controller/UserRoleController.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class UserRoleController extends AbstractController
 {
     #[Route('/user/role', name: 'app_user_role')]
+    #[IsGranted('ROLE_MEMBER')]
     public function index(): Response
     {
         return $this->render('user_role/index.html.twig', [
