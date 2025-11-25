77dd553f5348f2fd57f3cafa2ef4a3d94eea59d7
Hugo DESJARDINS <hdesj84@gmail.com>
2025-11-25 08:48:29 +0100

    ajout des routes CRUD pour la gestion des ouvrages du libraire

--- src/Controller/LibrarianController.php
+++ src/Controller/LibrarianController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Ouvrage;
+use App\Form\OuvrageType;
+use App\Repository\OuvrageRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -37,4 +42,98 @@ final class LibrarianController extends AbstractController
         return $this->render('librarian/members.html.twig');
     }
 
+    #[Route('/librarian/ouvrages', name: 'librarian_ouvrages')]
+    #[IsGranted('ROLE_LIBRARIAN')]
+    public function listOuvrages(OuvrageRepository $ouvrageRepository): Response
+    {
+        return $this->render('librarian/ouvrages/index.html.twig', [
+            'ouvrages' => $ouvrageRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/librarian/ouvrage/new', name: 'librarian_ouvrage_new')]
+    #[IsGranted('ROLE_LIBRARIAN')]
+    public function createOuvrage(Request $request, EntityManagerInterface $em): Response
+    {
+        $ouvrage = new Ouvrage();
+        $form = $this->createForm(OuvrageType::class, $ouvrage);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Gérer les champs non mappés manuellement
+            if ($form->has('Langues') && $form->get('Langues')->getData()) {
+                $ouvrage->setLangues($form->get('Langues')->getData());
+            }
+            if ($form->has('annee') && $form->get('annee')->getData()) {
+                $ouvrage->setAnnée($form->get('annee')->getData());
+            }
+            
+            $ouvrage->setCreatedBy($this->getUser());
+            $em->persist($ouvrage);
+            $em->flush();
+
+            $this->addFlash('success', 'L\'ouvrage a été créé avec succès.');
+            return $this->redirectToRoute('librarian_ouvrages');
+        }
+
+        return $this->render('librarian/ouvrages/form.html.twig', [
+            'form' => $form->createView(),
+            'ouvrage' => $ouvrage,
+            'isEdit' => false,
+        ]);
+    }
+
+    #[Route('/librarian/ouvrage/{id}/edit', name: 'librarian_ouvrage_edit')]
+    #[IsGranted('ROLE_LIBRARIAN')]
+    public function editOuvrage(Request $request, Ouvrage $ouvrage, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(OuvrageType::class, $ouvrage);
+        
+        // Pré-remplir les champs non mappés
+        if ($ouvrage->getLangues()) {
+            $form->get('Langues')->setData($ouvrage->getLangues());
+        }
+        if ($ouvrage->getAnnée()) {
+            $form->get('annee')->setData($ouvrage->getAnnée());
+        }
+        
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Gérer les champs non mappés manuellement
+            if ($form->has('Langues')) {
+                $ouvrage->setLangues($form->get('Langues')->getData());
+            }
+            if ($form->has('annee')) {
+                $ouvrage->setAnnée($form->get('annee')->getData());
+            }
+            
+            $em->flush();
+
+            $this->addFlash('success', 'L\'ouvrage a été modifié avec succès.');
+            return $this->redirectToRoute('librarian_ouvrages');
+        }
+
+        return $this->render('librarian/ouvrages/form.html.twig', [
+            'form' => $form->createView(),
+            'ouvrage' => $ouvrage,
+            'isEdit' => true,
+        ]);
+    }
+
+    #[Route('/librarian/ouvrage/{id}/delete', name: 'librarian_ouvrage_delete', methods: ['POST'])]
+    #[IsGranted('ROLE_LIBRARIAN')]
+    public function deleteOuvrage(Request $request, Ouvrage $ouvrage, EntityManagerInterface $em): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$ouvrage->getId(), $request->request->get('_token'))) {
+            $em->remove($ouvrage);
+            $em->flush();
+
+            $this->addFlash('success', 'L\'ouvrage a été supprimé avec succès.');
+        } else {
+            $this->addFlash('danger', 'Token CSRF invalide.');
+        }
+
+        return $this->redirectToRoute('librarian_ouvrages');
+    }
 }
